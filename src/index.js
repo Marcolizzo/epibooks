9c8513987f5b5082c8ea684483f42d5177bd66ab
Marco Santoro <msantor@hotmail.it>
2024-02-20 19:52:12 +0100

    Add redux and config

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const reducer = combineReducers({
+
+})
+
+const store = configureStore({
+  reducer
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
